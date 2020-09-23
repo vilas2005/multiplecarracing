ce6ac58eaeb18913da730dce701ea5534cc83f84
vilas2005 <68593708+vilas2005@users.noreply.github.com>
2020-09-23 13:48:54 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# multiplecarracing
+# synchronousBallMovement
+Ball moving synchronously
---
+++ sketch.js
@@ -0,0 +1,16 @@
+var canvas,backgroundImg,playerCount,database,form,player,game;
+var gameState = 0;
+function setup(){
+  canvas = createCanvas(400,400);
+  database = firebase.database();
+  game = new Game();
+  game.getState();
+  game.start();
+  
+
+
+}
+function draw(){
+
+}
+ 
